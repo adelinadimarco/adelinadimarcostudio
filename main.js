6e5a718149d77650beedcf935d1266010634f7cc
Ade <aderdimarco@gmail.com>
2026-05-04 18:21:26 -0300

    new svg mobile menu icons

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-const toggleBtn = document.querySelector('.menu-toggle');
-const body = document.body;
+const toggleBtn = document.getElementById('mobileToggle');
+const mobileMenu = document.getElementById('mobileMenu');
+const closeBtn = document.getElementById('closeMenu');
 
 toggleBtn.addEventListener('click', () => {
-  body.classList.toggle('mobile-menu');
+  mobileMenu.classList.toggle('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('active');
 });
